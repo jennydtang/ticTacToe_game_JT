cdd9c2182867bc6b8278bc5a156406b3095d63db
Jenny Tang <jennydtang@gmail.com>
2020-04-27 15:10:59 -0400

    Part 2 of lab completed

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -4,32 +4,67 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-class Square extends React.Component {
-  //parent component passes stated down to children by using props; keeps child components in sync with each other and parent
-  //add a constructor to initialize the state
-  constructor(props) {
-    super(props); //need to call a super when defining constructor of subclass; all react component classes have constructor that start with super(props)call
-    this.state = {
-      //this.state is private to a React component that it's defined in
-      value: null,
-    };
-  }
-  render(i) {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
+//class Square extends React.Component {
+//parent component passes stated down to children by using props; keeps child components in sync with each other and parent
+//add a constructor to initialize the state
+// constructor(props) {
+//   super(props); //need to call a super when defining constructor of subclass; all react component classes have constructor that start with super(props)call
+//   this.state = {
+//     //this.state is private to a React component that it's defined in
+//     value: null,
+//   };
+// }
+//No longer need constructor because square no longer keeps track of the game
+//render(i) {
+
+//function components are a simpler way to write components that only contain render method and don't have their own state
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true, //x is set as first move by default now (a boolean)
+    };
+  }
+
+  //Define handleClick
+  handleClick(i) {
+    const squares = this.state.squares.slice(); //.slice used to create a copy of squares array to modify
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      //need parenthesis so JS doesn't insert ; and break code
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
@@ -68,6 +103,25 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
